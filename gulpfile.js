5a32dc18779eba62f009b0ecf2e4a3ece92253bc
Simon Yu <simonyu@Simons-MacBook-Air.local>
2015-02-12 21:44:09 -0800

    initial project setup

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require("gulp"),
+    gulputil = require("gulp-util"),
+    watch = require("gulp-watch"),
+    concat = require("gulp-concat"),
+    uglify = require("gulp-uglify"),
+    sass = require("gulp-sass"),
+    bourbon = require("node-bourbon").includePaths,
+    neat = require("node-neat").includePaths,
+    minifyCSS = require("gulp-minify-css");
+
+gulp.task("scripts", function() {
+  gulp.src("js/*.js")
+      .pipe(concat("main.min.js"))
+      .pipe(uglify())
+      .pipe(gulp.dest("js/min"));
+});
+
+gulp.task("styles", function() {
+  gulp.src("scss/main.scss")
+      .pipe(sass({
+        includePaths: ["styles"].concat(bourbon)
+                                .concat(neat)
+      }))
+      .pipe(minifyCSS())
+      .pipe(gulp.dest("css/"));
+});
+
+gulp.task("watch", function() {
+  gulp.watch("js/*.js", ["scripts"]);
+  gulp.watch("scss/main.scss", ["styles"]);
+});
+
+gulp.task("default", ["scripts", "styles", "watch"]);
+
